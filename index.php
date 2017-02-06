f5fa57451c6ffff9ab594b4063fa05eaffce0dee
Valerio Sanguineti <vdrvs@users.noreply.github.com>
2017-02-06 14:34:54 +0100

    pure css

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 	//REF: http://www.redbeanphp.com/index.php
 	require_once('lib/rb.php');
-	R::setup( 'mysql:host=127.0.0.1;dbname=pa','pa', 'pressione' );
+	R::setup( 'mysql:host=127.0.0.1;dbname=a','pa', 'pressione' );
 	
 	$pg=(empty($_REQUEST['p'])) ? 'home' : $_REQUEST['p'];
 	$pg='pgs/'.$pg.'.php';
@@ -12,6 +12,18 @@
 	<head>
 		<meta charset="utf8" />
 		<title>Pressione Arteriosa</title>
+		<link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/pure-min.css" integrity="sha384-UQiGfs9ICog+LwheBSRCt1o5cbyKIHbwjWscjemyBMT9YCUMZffs6UqUTd0hObXD" crossorigin="anonymous">		
+		<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.13/css/jquery.dataTables.min.css" />
+		<style>
+			body{font:12px 'Tahoma','Verdana','Arial',sens-serif;padding:.75em;margin:0 auto}
+			tfoot td{background:royalblue!important;color:white}
+		</style>
+		<script
+		  src="https://code.jquery.com/jquery-3.1.1.min.js"
+		  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+		  crossorigin="anonymous">
+		</script>
+		<script src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
 	</head>
 	<body>
 		<?php if (file_exists($pg)) include_once($pg); ?>
--- pgs/elenco.php
+++ pgs/elenco.php
@@ -1,37 +1,75 @@
 <?php
+	$valid=false;
+	$msg=null;
 	$table='pressione';
 	$record=(empty($_REQUEST['id'])) ?  R::dispense($table) : R::load($table, intval($_REQUEST['id']));	
 	try {
-		if ($record && !empty($_REQUEST['act']) && $_REQUEST['act']=='del') R::trash($record);
+		if ($record && !empty($_REQUEST['act']) && $_REQUEST['act']=='del') { 
+			$msg='Record # '.$record->id.' eliminato correttamente.';
+			R::trash($record);
+		}
 		if (!empty($_POST['datamisurazione'])){
 			foreach ($_POST as $k=>$v){
-				$record[$k]=$_POST[$k];
+				$record[$k]=$v;
 			}
-			R::store($record);
+			
+			$valid=$record['diastolica']<$record['sistolica'];
+			$valid=$valid && $record['datamisurazione']<=date('Y-m-d');
+			$valid=$valid && strtotime($record['datamisurazione']);
+			
+			if ($valid){
+				R::store($record);
+				$msg='Dati inseriti correttamente';
+			}else{
+				$msg='Dati non validi: '.json_encode($record);
+			}
 		}
 	} catch (RedBeanPHP\RedException\SQL $e) {
-		?>
-		<h4 class="msg label error">
-			<?=$e->getMessage()?>
-		</h4>
-		<?php
-	}	
-	$pa=R::find($table);
+		$msg=$e->getMessage();
+	}
+	
+	//$query='1';
+	$dal=(empty($_POST['dal'])) ? date('Y-m-d') : $_POST['dal'];
+	$al=(empty($_POST['al'])) ? date('Y-m-d') : $_POST['al'];
+	$query="datamisurazione BETWEEN '".$dal."' AND '".$al."' ";
+	
+	$pa=R::find($table,$query);
+	$media=R::getRow('SELECT avg(sistolica) AS sistolica, avg(diastolica) AS diastolica, avg(peso) AS peso FROM pressione WHERE '.$query);
 ?>
 <h2>
 	<a href="index.php">
 		Misurazioni
 	</a>
 </h2>
-<form action="?p=elenco" method="POST">
+	<p>
+		<form action="?p=elenco" method="POST" class="pure-form">
+			Dal 
+			<input type="date" name="dal" value="<?=$dal?>" max="<?=date('Y-m-d')?>"/>
+			&nbsp; al 
+			<input type="date" name="al" value="<?=$al?>" max="<?=date('Y-m-d')?>"/>
+			<button type="submit">Mostra</button>
+		</form>
+	</p>
+<h4 id="message" class="msg label error">
+	<?=$msg?>
+</h4>
+<form action="?p=elenco" method="POST" onsubmit="validateForm()" class="pure-form">
 	<caption>Nuova misurazione:</caption>
-	<input type="date" name="datamisurazione" value="<?=date('Y-m-d');?>" placeholder="data" required />
-	<input type="number" name="sistolica" placeholder="sistolica" required />
-	<input type="number" name="diastolica" placeholder="diastolica" required />	
-	<button type="submit">Salva</button>
+	<input 
+		type="date" 
+		name="datamisurazione" 
+		value="<?=date('Y-m-d')?>" 
+		max="<?=date('Y-m-d')?>" 
+		placeholder="data" 
+		required 
+	/>
+	<input type="number" name="sistolica" placeholder="sistolica" required onchange="limit(this)" />
+	<input type="number" name="diastolica" placeholder="diastolica" required />		
+	<input type="number" name="peso" placeholder="peso" step="any" />	
+	<button type="submit" class="pure-button pure-button-primary">Salva</button>
 </form>
-<table border="1">
-	<thead>
+<table id="tabmisura" class="pure-table pure-table-striped pure-table-bordered">
+	<thead>	
 		<tr>
 			<th>
 				Data misurazione
@@ -43,6 +81,9 @@
 				Diastolica
 			</th>
 			<th>
+				Peso
+			</th>
+			<th>
 				&nbsp;
 			</th>			
 		</tr>
@@ -51,7 +92,7 @@
 	<?php foreach ($pa as $i) : ?>
 		<tr>
 			<td>
-				<?=$i->datamisurazione?>
+				<?=date_format(date_create($i->datamisurazione),'d/m/Y')?>
 			</td> 
 			<td>
 				<?=$i->sistolica?> 
@@ -60,9 +101,57 @@
 				<?=$i->diastolica?>
 			</td>
 			<td>
+				<?=$i->peso?>
+			</td>
+			<td>
 				<a href="?p=elenco&act=del&id=<?=$i->id?>" title="elimina questa rilevazione">x</a>
 			</td>			
 		</tr>
 	<?php endforeach; ?>
 	</tbody>
+	<tfoot>
+		<tr>
+			<td>
+				Media:
+			</td> 
+			<td>
+				<?=number_format($media['sistolica'],0)?> 
+			</td> 
+			<td>
+				<?=number_format($media['diastolica'],0)?> 
+			</td>
+			<td>
+				<?=number_format($media['peso'],2)?> 
+			</td>
+			<td>
+				<a href="?p=elenco&act=del&id=<?=$i->id?>" title="elimina questa rilevazione">x</a>
+			</td>			
+		</tr>
+	</tfoot>	
 </table>
+<script>
+	var limit=function(e){
+		var inp=document.getElementsByName('diastolica')[0]
+		inp.setAttribute('max',e.value)
+	}
+	var validateForm=function(e){
+		var valid=false
+		var data={diastolica:false,sistolica:false,datamisurazione:false}
+		data.diastolica=document.getElementsByName('diastolica')[0].value || false
+		data.sistolica=document.getElementsByName('sistolica')[0].value || false
+		data.datamisurazione=document.getElementsByName('datamisurazione')[0].value || false
+		valid=parseInt(data.diastolica)<parseInt(data.sistolica)
+		valid=valid && new Date(data.datamisurazione)<=new Date()
+		if (!valid) {
+			document.getElementById('message').innerHtml('Controlla i dati inseriti, perché non sono validi. <br />'+JSON_encode(data))
+			event.preventDefault()
+		}
+	}
+</script>
+<script>
+	$(function() {
+		$('form').addClass('pure-form')
+		$('button').addClass('pure-button')
+		$('#tabmisura').DataTable()
+	})
+</script>
